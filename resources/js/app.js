5b1134a4f5ed0bb9ec0a1d3cbc3694981d108cfb
LeviathanDeivis <margisdeividas@gmail.com>
2026-03-08 21:07:58 +0200

    Updated .gitignore file; added bootstrap and FullCalendar extensions

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,7 @@
-import './bootstrap';
+import 'bootstrap';
+
+import { Calendar } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+
+window.Calendar = Calendar;
+window.dayGridPlugin = dayGridPlugin;
